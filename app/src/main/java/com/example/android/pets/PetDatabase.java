52f34850b6937a6a1c8f4c41ef4cfaed92320b30
Eja Budiawan <reza.budiawan@gmail.com>
2019-10-01 19:29:30 +0700

    create pet database

---
+++ app/src/main/java/com/example/android/pets/PetDatabase.java
@@ -0,0 +1,28 @@
+package com.example.android.pets;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {PetEntity.class}, version = 1)
+public abstract class PetDatabase extends RoomDatabase {
+
+    private static PetDatabase instance;
+
+    public abstract PetDao petDao();
+
+    public static synchronized PetDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(
+                    context.getApplicationContext(),
+                    PetDatabase.class,
+                    "pet_database")
+                    .fallbackToDestructiveMigration()
+                    .allowMainThreadQueries() // SHOULD NOT BE USED IN PRODUCTION !!!
+                    .build();
+        }
+        return instance;
+    }
+}
